0b0d3a9bf1a48a8fb3792809bfc214e4baeb8841
Jan Trejbal <jan.trejbal@gmail.com>
2017-01-02 23:55:54 +0100

    Update before code

--- src/exceptions.php
+++ src/exceptions.php
@@ -1,28 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jam
- * Date: 13.2.15
- * Time: 16:54
- */
 
 namespace Trejjam\MailChimp;
 
+use Nette;
+use Trejjam;
 
-use Nette,
-	Trejjam;
-
-interface Exception
-{
-
-}
-
-interface LogicalException extends Exception
-{
-
-}
-
-class LogicException extends \LogicException implements LogicalException
+class LogicException extends \LogicException
 {
 
 }
