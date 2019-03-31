36e4318ddb3563e2c41b0e4029af74a5b4b74f3b
Jan Trejbal <jan@trejbal.land>
2019-03-31 16:43:17 +0200

    Migrate extension configuration to typed one (instead of mixed arrays)

--- src/DI/MailChimpExtension.php
+++ src/DI/MailChimpExtension.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 
 namespace Trejjam\MailChimp\DI;
 
-use Composer\CaBundle\CaBundle;
 use GuzzleHttp;
 use Nette\DI\Compiler;
+use Nette\DI\Config\Expect;
+use Nette\DI\Config\Schema;
 use Nette\Utils\Strings;
 use Nette\Utils\Validators;
 use Trejjam\BaseExtension\DI\BaseExtension;
@@ -16,23 +17,11 @@ use function Safe\sprintf;
  * Inspired by
  * - https://github.com/zbycz/mailchimp-v3-php/blob/master/MailchimpService.php
  * - https://github.com/Kdyby/CsobPaymentGateway
+ *
+ * @property ExtensionConfiguration $config
  */
 final class MailChimpExtension extends BaseExtension
 {
-    protected $default = [
-        'findDataCenter' => true,
-        'apiUrl'         => 'https://%s.api.mailchimp.com/%s/',
-        'apiKey'         => null,
-        'lists'          => [],  // self named ids from https://<dc>.api.mailchimp.com/playground/
-        'segments'       => [],
-        'http'           => [
-            'clientFactory' => null,
-            'client'        => [
-                'verify' => null, //NULL will be filled by Composer CA
-            ],
-        ],
-    ];
-
     protected $classesDefinition = [
         'http.client' => GuzzleHttp\Client::class,
 
@@ -50,9 +39,35 @@ final class MailChimpExtension extends BaseExtension
 
     ];
 
+    /**
+     * pre Nette 3.0 compatibility
+     *
+     * @var ExtensionConfiguration
+     */
+    private $shadowConfig;
+
     public function __construct()
     {
-        $this->default['http']['client']['verify'] = CaBundle::getSystemCaRootBundlePath();
+        $this->config = new ExtensionConfiguration();
+
+        if (!method_exists(parent::class, 'getConfigSchema')) {
+            // pre Nette 3.0 compatibility
+            $this->shadowConfig = $this->config;
+        }
+    }
+
+    public function getConfigSchema() : Schema
+    {
+        return Expect::from($this->config)->normalize(
+            function (array $config) {
+                if (true === ($config['findDataCenter'] ?? $this->config->findDataCenter)) {
+                    // unable to find, possible use of neon parameter, which will be expanded later
+                    $config['apiUrl'] = $this->config->apiUrlTemplate;
+                }
+
+                return $config;
+            }
+        );
     }
 
     /**
@@ -64,70 +79,97 @@ final class MailChimpExtension extends BaseExtension
      */
     public function loadConfiguration(bool $validateConfig = true) : void
     {
-        parent::loadConfiguration();
+        if (!method_exists(parent::class, 'getConfigSchema')) {
+            // pre Nette 3.0 compatibility
 
-        Validators::assert($this->config['apiUrl'], 'string', 'apiUrl');
-        Validators::assert($this->config['apiKey'], 'string', 'apiKey');
-        Validators::assert($this->config['lists'], 'array', 'list');
-        Validators::assert($this->config['segments'], 'array', 'segments');
-        Validators::assert($this->config['http']['clientFactory'], 'null|string|array|Nette\DI\Statement', 'http.client');
+            $config = (array)$this->shadowConfig;
+            $config['http'] = (array)$this->shadowConfig->http;
+            $this->validateConfig($config);
 
-        foreach ($this->config['lists'] as $listName => $listId) {
-            Validators::assert($listId, 'string', 'lists-' . $listName);
-        }
-        foreach ($this->config['segments'] as $listName => $segments) {
-            Validators::assertField($this->config['lists'], $listName);
+            Validators::assert($this->config['apiUrlTemplate'], 'string', 'apiUrl');
+            Validators::assert($this->config['apiKey'], 'string', 'apiKey');
+            Validators::assert($this->config['lists'], 'array', 'list');
+            Validators::assert($this->config['segments'], 'array', 'segments');
+            Validators::assert($this->config['http']['clientFactory'], 'null|string|array|Nette\DI\Statement', 'http.client');
 
-            foreach ($segments as $segmentId) {
-                Validators::assert($segmentId, 'string|integer', $listName);
+            foreach ($this->config['lists'] as $listName => $listId) {
+                Validators::assert($listId, 'string', 'lists-' . $listName);
             }
+
+            foreach ($this->config['segments'] as $listName => $segments) {
+                foreach ($segments as $segmentId) {
+                    Validators::assert($segmentId, 'string|integer', $listName);
+                }
+            }
+
+            if ($this->config['findDataCenter'] !== true) {
+                Validators::assert($this->config['apiUrl'], 'string', 'apiUrl');
+            }
+
+            $this->config = (object) $this->config;
+            $this->config->http = (object) $this->config->http;
         }
 
-        if ($this->config['findDataCenter'] === true) {
-            $accountDataCenter = Strings::match($this->config['apiKey'], '~-(us(?:\d+))$~');
-            $this->config['apiUrl'] = sprintf($this->config['apiUrl'], $accountDataCenter[1], MailChimp\Request::VERSION);
+        if ($this->config->findDataCenter === true) {
+            $accountDataCenter = Strings::match($this->config->apiKey, '~-(us(?:\d+))$~');
+            $this->config->apiUrl = sprintf($this->config->apiUrlTemplate, $accountDataCenter[1], MailChimp\Request::VERSION);
         }
 
-        Validators::assert($this->config['apiUrl'], 'string', 'apiUrl');
+        foreach (array_keys($this->config->segments) as $listName) {
+            Validators::assertField($this->config->lists, $listName);
+        }
     }
 
     public function beforeCompile() : void
     {
+        parent::loadConfiguration(false);
+
         parent::beforeCompile();
 
         $types = $this->getTypes();
 
-        if ($this->config['http']['clientFactory'] !== null) {
-            if (is_string($this->config['http']['clientFactory']) && Strings::startsWith($this->config['http']['clientFactory'], '@')) {
-                $types['http.client']->setFactory($this->config['http']['clientFactory']);
+        if ($this->config->http->clientFactory !== null) {
+            if (is_string($this->config->http->clientFactory) && Strings::startsWith($this->config->http->clientFactory, '@')) {
+                $types['http.client']->setFactory($this->config->http->clientFactory);
             }
             else {
-                Compiler::loadDefinition($types['http.client'], $this->config['http']['clientFactory']);
+                if (!method_exists($this, 'loadDefinitionsFromConfig')) {
+                    // pre Nette 3.0 compatibility
+
+                    Compiler::loadDefinition($types['http.client'], $this->config->http->clientFactory);
+                }
+                else {
+                    $this->loadDefinitionsFromConfig(
+                        [
+                            'http.client' => $this->config->http->clientFactory,
+                        ]
+                    );
+                }
             }
         }
 
         $types['http.client']->setArguments(
             [
-                'config' => $this->config['http']['client'],
+                'config' => $this->config->http->client,
             ]
         )->setAutowired(false);
 
         $types['request']->setArguments(
             [
                 'httpClient' => $this->prefix('@http.client'),
-                'apiUrl'     => $this->config['apiUrl'],
-                'apiKey'     => $this->config['apiKey'],
+                'apiUrl'     => $this->config->apiUrl,
+                'apiKey'     => $this->config->apiKey,
             ]
         );
 
         $types['lists']->setArguments(
             [
-                'lists' => $this->config['lists'],
+                'lists' => $this->config->lists,
             ]
         );
         $types['segments']->setArguments(
             [
-                'segments' => $this->config['segments'],
+                'segments' => $this->config->segments,
             ]
         );
     }
--- tests/cases/DITest.phpt
+++ tests/cases/DITest.phpt
@@ -5,6 +5,7 @@ namespace Trejjam\MailChimp\Tests;
 
 use GuzzleHttp;
 use Nette\DI\Compiler;
+use Nette\DI\Definitions\Reference;
 use Tester\TestCase;
 use Tester\Assert;
 use Trejjam\MailChimp\DI\MailChimpExtension;
@@ -14,95 +15,139 @@ $container = require __DIR__ . '/../bootstrap.php';
 
 final class DITest extends TestCase
 {
-    public function testConfig() : void
-    {
-        $mailChimpExtension = new MailChimpExtension;
-        $mailChimpExtension->setCompiler(new Compiler, 'container_' . __FUNCTION__);
+	private const NAME = 'trejjam.ares';
 
-        $mailChimpExtension->setConfig(
-            [
-                'apiKey'   => 'someApiKey123-us11',
-                'lists'    => [
-                    'newsletter' => 'foo123',
-                    'user'       => '123',
-                ],
-                'segments' => [
-                    'newsletter' => [
-                        'segmentA' => 2,
-                    ],
-                ],
-            ]
-        );
+	public function testConfig() : void
+	{
+		$mailChimpExtension = new MailChimpExtension;
 
-        $mailChimpExtension->loadConfiguration();
+		$compiler = new Compiler;
+		$compiler->addExtension(self::NAME, $mailChimpExtension);
+		$compiler->addConfig(
+			[
+				self::NAME => [
+					'apiKey'   => 'someApiKey123-us11',
+					'lists'    => [
+						'newsletter' => 'foo123',
+						'user'       => '123',
+					],
+					'segments' => [
+						'newsletter' => [
+							'segmentA' => 2,
+						],
+					],
+				],
+			]
+		);
+		$compiler->processExtensions();
 
-        $mailChimpConfig = $mailChimpExtension->getConfig();
+		$mailChimpConfig = $mailChimpExtension->getConfig();
 
-        Assert::same(
-            [
-                'findDataCenter' => true,
-                'apiUrl'         => 'https://us11.api.mailchimp.com/3.0/',
-                'apiKey'         => 'someApiKey123-us11',
-                'lists'          => [
-                    'newsletter' => 'foo123',
-                    'user'       => '123',
-                ],
-                'segments'       => [
-                    'newsletter' => [
-                        'segmentA' => 2,
-                    ],
-                ],
-                'http'           => [
-                    'clientFactory' => null,
-                    'client'        => ['verify' => CaBundle::getSystemCaRootBundlePath()],
-                ],
-            ], $mailChimpConfig
-        );
-    }
+		Assert::same(true, $mailChimpConfig->findDataCenter);
+		Assert::same('https://us11.api.mailchimp.com/3.0/', $mailChimpConfig->apiUrl);
+		Assert::same('someApiKey123-us11', $mailChimpConfig->apiKey);
+		Assert::same(
+			[
+				'newsletter' => 'foo123',
+				'user'       => '123',
+			],
+			$mailChimpConfig->lists
+		);
+		Assert::same(
+			[
+				'newsletter' => [
+					'segmentA' => 2,
+				],
+			], $mailChimpConfig->segments
+		);
+		Assert::null($mailChimpConfig->http->clientFactory);
+		Assert::same(['verify' => CaBundle::getSystemCaRootBundlePath()], $mailChimpConfig->http->client);
+	}
 
-    public function testGuzzleFactory() : void
-    {
-        $compiler = new Compiler;
-        $containerBuilder = $compiler->getContainerBuilder();
+	public function testGuzzleFactory() : void
+	{
+		$mailChimpExtension = new MailChimpExtension;
 
-        $guzzleClassFactory = $containerBuilder->addDefinition('guzzleClassFactory');
-        $guzzleClassFactory->setType(GuzzleHttp\Client::class);
+		$compiler = new Compiler;
+		$compiler->addExtension(self::NAME, $mailChimpExtension);
+		$compiler->addConfig(
+			[
+				self::NAME => [
+					'apiKey' => 'someApiKey123-us11',
+					'http'   => [
+						'clientFactory' => '@guzzleClassFactory',
+					],
+				],
+			]
+		);
+		$containerBuilder = $compiler->getContainerBuilder();
 
-        $mailChimpExtension = new MailChimpExtension;
-        $extensionName = 'container_' . __FUNCTION__;
-        $mailChimpExtension->setCompiler($compiler, $extensionName);
+		$guzzleClassFactory = $containerBuilder->addDefinition('guzzleClassFactory');
+		$guzzleClassFactory->setType(GuzzleHttp\Client::class);
 
-        $mailChimpExtension->setConfig(
-            [
-                'apiKey' => 'someApiKey123-us11',
-                'http'   => [
-                    'clientFactory' => '@guzzleClassFactory',
-                ],
-            ]
-        );
+		$compiler->processExtensions();
 
-        $mailChimpExtension->loadConfiguration();
-        $mailChimpExtension->beforeCompile();
+		$mailChimpExtension->beforeCompile();
 
-        $mailChimpConfig = $mailChimpExtension->getConfig();
+		$mailChimpConfig = $mailChimpExtension->getConfig();
 
-        Assert::same(
-            [
-                'findDataCenter' => true,
-                'apiUrl'         => 'https://us11.api.mailchimp.com/3.0/',
-                'apiKey'         => 'someApiKey123-us11',
-                'lists'          => [],
-                'segments'       => [],
-                'http'           => [
-                    'clientFactory' => '@guzzleClassFactory',
-                    'client'        => ['verify' => CaBundle::getSystemCaRootBundlePath()],
-                ],
-            ], $mailChimpConfig
-        );
+		Assert::same(true, $mailChimpConfig->findDataCenter);
+		Assert::same('https://us11.api.mailchimp.com/3.0/', $mailChimpConfig->apiUrl);
+		Assert::same('someApiKey123-us11', $mailChimpConfig->apiKey);
+		Assert::same([], $mailChimpConfig->lists);
+		Assert::same([], $mailChimpConfig->segments);
+		Assert::same('@guzzleClassFactory', $mailChimpConfig->http->clientFactory);
+		Assert::same(['verify' => CaBundle::getSystemCaRootBundlePath()], $mailChimpConfig->http->client);
 
-        $httpClient = $containerBuilder->getDefinition($extensionName . '.http.client');
-        Assert::same('@guzzleClassFactory', $httpClient->getFactory()->getEntity());
-    }
+		$httpClient = $containerBuilder->getDefinition(self::NAME . '.http.client');
+
+		$httpClientServiceDefinition = $httpClient->getFactory()->getEntity();
+		if ($httpClientServiceDefinition instanceof Reference) {
+			Assert::same('guzzleClassFactory', $httpClientServiceDefinition->getValue());
+		}
+		else {
+			// pre Nette 3.0 compatibility
+			Assert::same('@guzzleClassFactory', $httpClientServiceDefinition);
+		}
+	}
+
+	public function testGuzzleFactory2() : void
+	{
+		$mailChimpExtension = new MailChimpExtension;
+
+		$compiler = new Compiler;
+		$compiler->addExtension(self::NAME, $mailChimpExtension);
+		$compiler->addConfig(
+			[
+				self::NAME => [
+					'apiKey' => 'someApiKey123-us11',
+					'http'   => [
+						'clientFactory' => 'GuzzleHttp\Client([])',
+					],
+				],
+			]
+		);
+
+		$compiler->processExtensions();
+
+		$mailChimpExtension->beforeCompile();
+
+		$mailChimpConfig = $mailChimpExtension->getConfig();
+
+		Assert::same(true, $mailChimpConfig->findDataCenter);
+		Assert::same('https://us11.api.mailchimp.com/3.0/', $mailChimpConfig->apiUrl);
+		Assert::same('someApiKey123-us11', $mailChimpConfig->apiKey);
+		Assert::same([], $mailChimpConfig->lists);
+		Assert::same([], $mailChimpConfig->segments);
+		Assert::same('GuzzleHttp\Client([])', $mailChimpConfig->http->clientFactory);
+		Assert::same(['verify' => CaBundle::getSystemCaRootBundlePath()], $mailChimpConfig->http->client);
+
+		$containerBuilder = $compiler->getContainerBuilder();
+		$httpClient = $containerBuilder->getDefinition(self::NAME . '.http.client');
+
+		$httpClientServiceDefinition = $httpClient->getFactory()->getEntity();
+		Assert::same('GuzzleHttp\Client([])', $httpClientServiceDefinition);
+	}
 }
 
 $test = new DITest($container);
