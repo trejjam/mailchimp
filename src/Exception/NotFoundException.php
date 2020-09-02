e91fc7284b7d4cdfd2a6df50ea0df0e16375d4b2
Jan Trejbal <jan@trejbal.land>
2020-09-02 15:05:08 +0200

    Fix phpstan errors

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 namespace Trejjam\MailChimp\Exception;
 
 use Exception;
+use Throwable;
 
 abstract class NotFoundException extends \LogicException
 {
-    public function __construct($message = '', $code = 0, ?\Throwable $previous = null)
+    /**
+     * @param string $message
+     * @param int|Throwable $code
+     */
+    public function __construct($message = '', $code = 0, ?Throwable $previous = null)
     {
         if ($code instanceof Exception && $previous === null) {
             $previous = $code;
             $code = 0;
         }
+        assert(is_int($code));
 
         parent::__construct($message, $code, $previous);
     }
