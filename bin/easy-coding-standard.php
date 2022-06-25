d80efb7680ff5f2a13233b1b518578d2ce8a0fe1
Jan Trejbal <jan@trejbal.land>
2022-06-25 14:06:37 +0200

    Update easy-coding-standard.php

--- bin/easy-coding-standard.php
+++ bin/easy-coding-standard.php
@@ -6,27 +6,27 @@ use PhpCsFixer\Fixer\Basic\BracesFixer;
 use PhpCsFixer\Fixer\FunctionNotation\ReturnTypeDeclarationFixer;
 use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 $rootDir = __DIR__ . '/../';
 
-return static function (ContainerConfigurator $containerConfigurator) use ($rootDir) : void {
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/array.php');
+return static function (ECSConfig $ecsConfig) use ($rootDir) : void {
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/array.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/control-structures.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/control-structures.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/docblock.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/docblock.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/namespaces.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/namespaces.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/strict.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/strict.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/clean-code.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/clean-code.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/psr12.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/psr12.php');
 
-    $parameters = $containerConfigurator->parameters();
+    $parameters = $ecsConfig->parameters();
 
     $parameters->set(Option::INDENTATION, Option::INDENTATION_SPACES);
 
@@ -37,10 +37,7 @@ return static function (ContainerConfigurator $containerConfigurator) use ($root
         ScopeIndentSniff::class => null,
     ]);
 
-    $services = $containerConfigurator->services();
-
-    $services->set(ReturnTypeDeclarationFixer::class)
-        ->call('configure', [[
-            'space_before' => 'one',
-        ]]);
+    $ecsConfig->ruleWithConfiguration(ReturnTypeDeclarationFixer::class, [
+        'space_before' => 'one',
+    ]);
 };
